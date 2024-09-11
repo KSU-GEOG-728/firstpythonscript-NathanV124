b4f3d66595fbe52b1ff5e6f0a70aecf3ec62a037
NathanV124 <ntverrill@ksu.edu>
2024-09-11 15:28:53 -0500

    Update GitHub-FirstPythonScript.py

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -3,9 +3,9 @@
 
 """
     File name: GitHub-FirstPythonScript.py
-    Author: ENTER YOUR  NAME
-    Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Author: Nathan Verrill
+    Description:  This script calculates the total stream miles for all streams in the Flint Hills.
+    Date created: 09/11/2024
     Python Version: 3.9.16
 """
 
@@ -14,3 +14,9 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
+acrpy.env.workspace= "C:/Users/ntverrill/Documents/GitHub/firstpythonscript-NathanV124/GitHub-FirstPythonScript.gdb"
+
+selectRegion = arcpy.management.SelectLayerByAttribute("ks_ecoregions", "NEW_SELECTION","US_L3NAME = 'Flint Hills'")
+createBuffer = arcpy.analysis.Buffer('ks_ecoregions', 'buffer_area', "10 kilometers" )
+createClip = arcpy.analysis.Clip('ks_major_rivers', 'buffer_area', 'clipped_area')
+arcpy.management.CalculateField('clipped_area', 'TotalLength', '!shape.length!')
